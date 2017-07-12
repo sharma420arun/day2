2debf71a58e654b42ffa8d4915e37ac9d9a197dc
sharma420arun <arun.sharma@quantiphi.com>
2017-07-12 15:46:52 +0530

    code

---
+++ dictonary.py
@@ -0,0 +1,6 @@
+import sys
+l1={"mumbai":"maharashtra","ranchi":"Jharkhand","ahemdabad":"gujarat"}
+#st=str(raw_input("Enter a city:"))
+print(l1[str(raw_input("Enter a city:"))])
+#s=str(raw_input("Enter a state:"))
+print l1.keys()[l1.values().index(str(raw_input("Enter a state:")))]
